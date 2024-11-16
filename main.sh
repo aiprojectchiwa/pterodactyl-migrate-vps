45c5c02c85be4081e15b657808c33e826ab0176e
aiprojectchiwa <140908266+aiprojectchiwa@users.noreply.github.com>
2024-11-16 13:56:28 +0700

    cayankkk chiroo

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ TEMP_FILE="temp_script.sh.gz"
 DECOMPRESSED_FILE="temp_script.sh"
 
 curl -L -o "$TEMP_FILE" "$BASE_URL/$FILE"
-gzip -d "$TEMP_FILE"
+gunzip "$TEMP_FILE"
 chmod +x "$DECOMPRESSED_FILE"
 ./"$DECOMPRESSED_FILE"
 rm -f "$DECOMPRESSED_FILE"
